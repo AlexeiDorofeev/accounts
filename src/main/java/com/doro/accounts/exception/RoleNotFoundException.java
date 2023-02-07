65aeaf440c79a6ba875b791368cb579a692185f6
Alexei.Dorofeev <Alexei.Dorofeev@wolterskluwer.com>
2023-02-07 06:51:15 +0100

    finished default role

---
+++ src/main/java/com/doro/accounts/exception/RoleNotFoundException.java
@@ -0,0 +1,14 @@
+package com.doro.accounts.exception;
+
+public class RoleNotFoundException extends RuntimeException{
+    public RoleNotFoundException() {
+    }
+
+    public RoleNotFoundException(String message) {
+        super(message);
+    }
+
+    public RoleNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/com/doro/accounts/sevice/UserService.java
+++ src/main/java/com/doro/accounts/sevice/UserService.java
@@ -1,25 +1,33 @@
 package com.doro.accounts.sevice;
 
+import com.doro.accounts.exception.RoleNotFoundException;
 import com.doro.accounts.model.Role;
 import com.doro.accounts.model.User;
 import com.doro.accounts.repository.RoleRepository;
 import com.doro.accounts.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
 @Service
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final RoleRepository roleRepository;
 
+    @Value("${app.defaultRoleId}")
+    private Long defaultRoleId;
+
     public User signup(User user) {
         if (user.getRole() == null) {
-            user.setRole(new Role(2L, "DEFAULT"));
+            Role defaultRole = roleRepository.findById(defaultRoleId)
+                .orElseThrow(() -> new RoleNotFoundException("Default role not found"));
+            user.setRole(defaultRole);
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
